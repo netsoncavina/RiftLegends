1452d7fc9eff0ce9d04502f851fc34beb66eb9f5
Netson Cavina <netsoncavina@gmail.com>
2023-07-27 16:18:31 -0300

    Refatoração de código

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Appbar from "../../components/Appbar";
+import CarouselSection from "./CarouselSection";
+import { getChampionsData } from "../../api/api";
+
+interface Region {
+  name: string;
+}
+
+const Home = () => {
+  const [champions, setChampions] = useState([]);
+
+  useEffect(() => {
+    getChampionsData().then((champions) => {
+      setChampions(champions as any);
+    });
+  }, []);
+
+  const regions = [
+    {
+      name: "Noxus",
+    },
+    {
+      name: "Demacia",
+    },
+    {
+      name: "Ionia",
+    },
+  ];
+
+  return (
+    <>
+      <Appbar />
+      <ScrollView style={styles.container}>
+        {regions.map((region: Region) => {
+          return (
+            <CarouselSection
+              key={region.name}
+              regionName={region.name}
+              champions={champions}
+            />
+          );
+        })}
+      </ScrollView>
+    </>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1F1F1F",
+  },
+});
