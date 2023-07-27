cc6a5762c14e7f741252787e70201959130fd834
Netson Cavina <netsoncavina@gmail.com>
2023-07-27 15:50:16 -0300

    criação de arquivo contendo as regiões e campeões

---
+++ src/utils/regions.tsx
@@ -0,0 +1,48 @@
+export const NoxusChampions = [
+  "Darius",
+  "Draven",
+  "Katarina",
+  "LeBlanc",
+  "Mordekaiser",
+  "Riven",
+  "Sion",
+  "Swain",
+  "Talon",
+  "Urgot",
+  "Vladimir",
+  "Kled",
+];
+
+export const DemaciaChampions = [
+  "Fiora",
+  "Garen",
+  "Jarvan IV",
+  "Lucian",
+  "Lux",
+  "Poppy",
+  "Quinn",
+  "Sona",
+  "Vayne",
+  "Xin Zhao",
+  "Shyvana",
+  "Galio",
+  "Kayle",
+  "Morgana",
+  "Sylas",
+];
+
+export const IoniaChampions = [
+  "Ahri",
+  "Akali",
+  "Irelia",
+  "Jhin",
+  "Karma",
+  "Kayn",
+  "Lee Sin",
+  "Master Yi",
+  "Shen",
+  "Varus",
+  "Yasuo",
+  "Yone",
+  "Zed",
+];
