cd408547a5f1705e1786d0b5f93064a5e5d63ff6
Netson Cavina <netsoncavina@gmail.com>
2023-07-29 01:15:50 -0300

    Adicionadas rotas

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, StatusBar } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
 import { useFonts } from "expo-font";
 import "react-native-gesture-handler";
 import Appbar from "./src/components/Appbar";
 import Home from "./src/pages/Home/Home";
-
+import ChampionPage from "./src/pages/ChampionPage/ChampionPage";
 interface ILanguage {
   languageCode: string;
   setLanguage: React.Dispatch<React.SetStateAction<any>>;
 }
+
+export type RootStackParamList = {
+  Home: undefined;
+  ChampionPage: undefined;
+};
+
 export default function App() {
   const [language, setLanguage] = useState<ILanguage>("de_DE" as any);
 
@@ -24,6 +31,7 @@ export default function App() {
   }
 
   const Drawer = createDrawerNavigator();
+  const Stack = createNativeStackNavigator();
   return (
     <PaperProvider>
       <StatusBar barStyle="light-content" backgroundColor="#1F1F1F" />
@@ -38,6 +46,13 @@ export default function App() {
           <Drawer.Screen
             name="Home"
             children={() => <Home language={language as any} />}
+          />
+          <Drawer.Screen
+            name="ChampionPage"
+            children={() => <ChampionPage />}
+            options={{
+              drawerItemStyle: { display: "none" },
+            }}
           />
         </Drawer.Navigator>
       </NavigationContainer>
