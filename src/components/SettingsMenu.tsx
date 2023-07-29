5147b9cb5f8495d79e5ecd83dd16b2d8a78017ab
Netson Cavina <netsoncavina@gmail.com>
2023-07-28 23:54:38 -0300

    Adição do controle de exibição do modal

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -2,33 +2,57 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Menu, MenuItem, MenuDivider } from "react-native-material-menu";
 import { Feather } from "@expo/vector-icons";
+import LanguageModal from "./LanguageModal";
 
 const SettingsMenu = () => {
   const [visible, setVisible] = useState(false);
-
   const hideMenu = () => setVisible(false);
-
   const showMenu = () => setVisible(true);
 
+  const [languageModalVisible, setLanguageModalVisible] = useState(false);
+
   return (
-    <Menu
-      visible={visible}
-      anchor={
-        <Feather
-          name="settings"
-          size={30}
-          color="#C89B3C"
-          onPress={() => setVisible(true)}
-        />
-      }
-      onRequestClose={hideMenu}
-    >
-      <MenuItem onPress={hideMenu}>Menu item 1</MenuItem>
-      <MenuItem onPress={hideMenu}>Menu item 2</MenuItem>
-      <MenuItem disabled>Disabled item</MenuItem>
-      <MenuDivider />
-      <MenuItem onPress={hideMenu}>Menu item 4</MenuItem>
-    </Menu>
+    <>
+      <Menu
+        visible={visible}
+        anchor={
+          <Feather
+            name="settings"
+            size={30}
+            color="#C89B3C"
+            onPress={() => setVisible(true)}
+          />
+        }
+        onRequestClose={hideMenu}
+        style={{
+          backgroundColor: "#1F1F1F",
+          width: 200,
+        }}
+      >
+        <MenuItem
+          onPress={() => {
+            setLanguageModalVisible(true);
+            hideMenu();
+          }}
+          style={{
+            width: 200,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          textStyle={{
+            color: "#C89B3C",
+          }}
+        >
+          Selecionar idioma
+        </MenuItem>
+      </Menu>
+
+      <LanguageModal
+        visible={languageModalVisible}
+        setVisible={setLanguageModalVisible}
+      />
+    </>
   );
 };
 
