b37c6618e52bd3f601938603d9f9f72da64c0f02
Netson Cavina <netsoncavina@gmail.com>
2023-07-29 00:51:43 -0300

    Refatoração na estrutura e adição do controle de idioma

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, StatusBar } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
 import { useFonts } from "expo-font";
 import "react-native-gesture-handler";
+import Appbar from "./src/components/Appbar";
 import Home from "./src/pages/Home/Home";
 
+interface ILanguage {
+  languageCode: string;
+  setLanguage: React.Dispatch<React.SetStateAction<any>>;
+}
 export default function App() {
+  const [language, setLanguage] = useState<ILanguage>("de_DE" as any);
+
   const [load] = useFonts({
     "League of Legends": require("./src/assets/fonts/Friz Quadrata Regular.ttf"),
   });
@@ -15,19 +22,23 @@ export default function App() {
   if (!load) {
     return null;
   }
-  const Drawer = createDrawerNavigator();
 
+  const Drawer = createDrawerNavigator();
   return (
     <PaperProvider>
       <StatusBar barStyle="light-content" backgroundColor="#1F1F1F" />
       <NavigationContainer>
+        <Appbar language={language as any} setLanguage={setLanguage as any} />
         <Drawer.Navigator
           initialRouteName="Home"
           screenOptions={{
             headerShown: false,
           }}
         >
-          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen
+            name="Home"
+            children={() => <Home language={language as any} />}
+          />
         </Drawer.Navigator>
       </NavigationContainer>
     </PaperProvider>
